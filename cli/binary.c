e41092e1f6f792bcc48e3be820ab8ee3818388e4
thelazybastard <256585194+thelazybastard@users.noreply.github.com>
2026-06-17 17:49:09 +0700

    simplified linked list insertion

--- cli/binary.c
+++ cli/binary.c
@@ -10,7 +10,6 @@ typedef struct node {
 
 void binary_conversion(int num) {
     node *current_ptr = NULL;
-    bool first_round = true;
     int num_store = num;
 
     while (true) {
@@ -21,26 +20,16 @@ void binary_conversion(int num) {
         int remainder = num_store % 2;
         num_store /= 2;
 
-        if (first_round) {
-            node *new_node = malloc(sizeof(node));
-            new_node->value = remainder;
-            new_node->next = NULL;
-            current_ptr = new_node;
-            first_round = false;
-        } else {
-            node *tmp = current_ptr;
-            node *new_node = malloc(sizeof(node));
-            new_node->value = remainder;
-            new_node->next = tmp;
-            current_ptr = new_node;
-        }
+        node *new_node = malloc(sizeof(node));
+        new_node->value = remainder;
+        new_node->next = current_ptr;
+        current_ptr = new_node;   
     }
 
     printf("The result in binary is: ");
     while (current_ptr != NULL) {
-        node *tmp = current_ptr;
         printf("%d", current_ptr->value);
-        current_ptr = tmp->next;
+        current_ptr = current_ptr->next;
     }
     printf("\n");
 }
